b84dbb1e0c63731168bce620f32b04b89c6ac4ae
Anastasiia Naubreit <fayst2996317@mail.ru>
2023-11-20 10:01:33 +0300

    project completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { postApi } from './shared/api/PostApi';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './app/App';
+import './index.css';
+
+const store = configureStore({
+  reducer: {
+    [postApi.reducerPath]: postApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postApi.middleware),
+});
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
---
+++ src/pages/Notfoundpage.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+ 
+const Notfoundpage = () => {
+    return (
+        <div>
+            Страница не существует. Нажми <Link to = ''>Перейти в блог</Link>
+        </div>
+    )
+}
+
+export { Notfoundpage }
