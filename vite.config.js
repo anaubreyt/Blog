7faf1e3c541d2e4e9bd02d1bfaed6e6a3283d5c5
Anastasiia Naubreit <fayst2996317@mail.ru>
2023-11-20 11:11:49 +0300

    добавила деплой

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-  },
+  base: '/Blog/',
 });
